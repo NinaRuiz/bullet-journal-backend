38ddad2278c4f4985a1a01ec96da4a98ebce3d48
Nina Ruiz <nina.ruizandres@gmail.com>
2020-04-11 18:58:19 +0200

    Connected with MongoDB database

--- index.js
+++ index.js
@@ -9,12 +9,20 @@ const routes = require('./routes/v1');
 
 const app = express();
 
-console.log('MONGO', process.env.MONGO);
-
 routes(app);
 
 const PORT = process.env.PORT || 4000;
 
-app.listen(PORT, () =>{
-    console.log('running on '+ PORT);
-});
+mongoose.connect(process.env.MONGO, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(
+    console.log('conected to mongdb'),
+    app.listen(PORT, () =>{
+    console.log('running on '+ PORT)
+    }),
+).catch(error =>{
+    console.log('mongodb error', error)
+    });
+
+
