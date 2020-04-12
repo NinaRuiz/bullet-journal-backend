fda97e3e14cac4fc13cefc203099199c642e3e86
Nina Ruiz <nina.ruizandres@gmail.com>
2020-04-12 13:56:54 +0200

    Created years mongoose schema

---
+++ mongodb/models/years.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const {Schema} = mongoose;
+
+const yearsSchema = new Schema(
+    {year: {type: String, required: true}},
+    {notebook: {mongoose.Schema.Types.ObjectId, ref: 'notebook'}},
+    { timestamps: true }
+);
+
+const model = mongoose.model('year', yearsSchema);
+
+module.exports = model;
